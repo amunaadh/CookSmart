70618b87c800869c220e404fe0dcf82106c3ae08
amunaadh <41142475+amunaadh@users.noreply.github.com>
2019-08-20 10:50:19 +0545

    Add files via upload

---
+++ app/src/main/res/ClickListener.java
@@ -0,0 +1,12 @@
+package ellere.cooksmart;
+
+import android.view.View;
+
+/**
+ * Created by swikriti on 8/14/2019.
+ */
+
+public interface ClickListener {
+    public void itemClicked(View view , int position);
+    public void thumbnailClicked(View view , int position);
+}
